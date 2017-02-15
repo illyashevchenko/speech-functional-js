c34e9da4a7e2dbf3875941921e2ac90940e1c093
Illia Shevchenko <illyashevchenko@gmail.com>
2017-02-15 22:05:15 +0100

    Add Debugging examples

---
+++ debugging.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const { tap, pipe, map, filter, join, identity, prop, concat, insert, __, apply } = require('ramda');
+
+const logSimple = tap(console.log);
+const log = pipe(
+  concat,
+  insert(1, __, [JSON.stringify, console.log]),
+  apply(pipe),
+  tap
+);
+const idList = pipe(
+  map(prop('id')),
+  logSimple,
+  filter(identity),
+  log('After filter: '),
+  join('; ')
+);
+
+idList([
+  { id: '4' },
+  { id: '3' },
+  {},
+]);
